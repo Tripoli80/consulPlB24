156449cf7d2d3ac2bd7a53d1db82788a4a92600b
Tripoli80 <tripoli80@gmail.com>
2023-01-05 10:37:02 +0000

    first commit

--- src/routes/deal.js
+++ src/routes/deal.js
@@ -7,12 +7,12 @@ const router = express.Router();
 router.post("/getdeal", tryWrapper(getDeal));
 
 router.post("/", (req, res) => {
-    console.log("all: ", req.body);
+  console.log("all: ", req.body);
+  console.log("headers: ", req.headers);
+  console.log("data: ", req.data);
   console.log("params: ", req.params);
-    console.log("query: ", req.query);
+  console.log("query: ", req.query);
   console.log("req: ", req);
-    
-    
 });
 
 module.exports = router;
