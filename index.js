2b929e0e8df9c18ecbca9a9aeb34355fa9585767
Tripoli80 <tripoli80@gmail.com>
2023-01-07 17:27:22 +0000

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 
 app.use(logger(formatsLogger));
