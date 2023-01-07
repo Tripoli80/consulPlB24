81901e3cebc78b49fd41ab15b8a401b2ac6748e5
Tripoli80 <tripoli80@gmail.com>
2023-01-07 02:21:08 +0000

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const { errorHandler } = require("./src/helpers");
 const PORT = process.env.PORT || 80;
 
 // app.use(express.json());
-app.use(express.bodyParser());
+const bodyParser = require("body-parser");
+app.use(bodyParser);
+
 app.use(logger(formatsLogger));
 
 app.use(cors());
