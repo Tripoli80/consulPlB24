2ac0eb9aaf5539244f2ff3ec1377ba4883f438fe
Tripoli80 <tripoli80@gmail.com>
2023-01-07 03:36:01 +0000

    first commit

--- src/controllers/listenEvent.js
+++ src/controllers/listenEvent.js
@@ -1,9 +1,12 @@
+const { tryWrapper } = require("../helpers");
+const { getDealById } = require("../services/dealServices");
 const { getDeal } = require("./dealControllers");
 
 const listeningEvents = async (req, res) => {
   console.dir(req.body["data[FIELDS][ID]"]);
-  console.dir(req.body[" event"]);
-  const dealData = await tryWrapper(getDeal);
+  console.dir(req.body["event"]);
+  const idDeal = eq.body["data[FIELDS][ID]"];
+  const dealData = await tryWrapper(getDealById(idDeal));
   console.log("🚀 ", dealData);
 
   res.send("ok");
