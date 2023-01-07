438cfd8560d14676657807ad47c8dc1086138d50
Tripoli80 <tripoli80@gmail.com>
2023-01-07 17:31:01 +0000

    first commit

--- src/controllers/listenEvent.js
+++ src/controllers/listenEvent.js
@@ -1,16 +1,21 @@
+const { json } = require("body-parser");
 const { EVENT_ID } = require("../constans");
 const { tryWrapper } = require("../helpers");
 const { getDealById } = require("../services/dealServices");
 const { getDeal } = require("./dealControllers");
 
 const listeningEvents = async (req, res) => {
-  const { ts, event } = req.body;
+
+    const data =  JSON.parse(req.body)
+    console.log("🚀 ~ file: listenEvent.js:10 ~ listeningEvents ~ data", data)
+    const { ts, event } = req.body;
+    
   console.log("🚀 ~ ts +event", ts, EVENT_ID, new Date());
   if (EVENT_ID.includes(Number(ts))) {
     console.log("!!!!!!!!!!alredy do");
     return res.send("ok");
   }
-  console.log("🚀 ~ ts +event", ts, EVENT_ID, req);
+  console.log("🚀 ~ ts +event", ts, EVENT_ID);
 
   EVENT_ID.push(Number(ts));
 
