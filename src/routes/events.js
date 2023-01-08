1c7a208c371b2b7b83c14a87dc27151fbe9129bd
Tripoli80 <tripoli80@gmail.com>
2023-01-08 07:08:13 +0000

    first commit

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { EVENT_ID } = require("../constans");
 const { addPaymentToCallendar } = require("../controllers/callendar");
 const { listeningEvents } = require("../controllers/listenEvent");
 const { tryWrapper } = require("../helpers");
@@ -7,7 +8,20 @@ const router = express.Router();
 
 // router.post("/getdeal", tryWrapper(getDeal));
 
-router.post("/", tryWrapper(listeningEvents));
+router.post(
+  "/",
+  tryWrapper((req, res) => {
+    const {
+      body: { ts },
+    } = req;
+    console.log("🚀 ~ file: events.js:16 ~ ts", ts);
+    if (EVENT_ID.includes(Number(ts))) {
+      return res.status(210).send("already do");
+    }
+    EVENT_ID.push(Number(ts));
+    listeningEvents(req, res);
+  })
+);
 router.post("/cal", tryWrapper(addPaymentToCallendar));
 
 module.exports = router;
