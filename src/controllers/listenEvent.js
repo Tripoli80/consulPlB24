2a95fb0d156a45879684ac4ebde408585f229d41
Tripoli80 <tripoli80@gmail.com>
2023-01-08 05:58:58 +0000

    first commit

--- src/controllers/listenEvent.js
+++ src/controllers/listenEvent.js
@@ -6,19 +6,19 @@ const { addToCallendar } = require("../services/callendar");
 // const { tryWrapper } = require("../helpers");
 const { getDealById } = require("../services/dealServices");
 
-const listeningEvents = async (req, res) => {
+const listeningEvents =  (req, res) => {
   const { ts, event } = req.body;
   const idDeal = req.body["data[FIELDS][ID]"];
   EVENT_ID = [...EVENT_ID, Number(ts)];
+  console.log("🚀 EVENT_ID", EVENT_ID);
 
-  const dealData = await getDealById(Number(idDeal));
+  const dealData =  getDealById(Number(idDeal));
 
   const dates = dealData[process.env.ARR_PAY_DATE];
   const approve = dealData[process.env.APPROVE_TO_CALENDAR];
   const count = dealData[process.env.COUNT_PAYMANT];
   const name = dealData["TITLE"];
 
-  console.log("🚀 EVENT_ID", EVENT_ID);
   if (approve && dates.length > 0) {
     addToCallendar({ dates, count, approve, name, idDeal }).then(() => {
       res.send("ok");
