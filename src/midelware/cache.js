5a87b8446d46b294c0ecfdc01236ed16f2920a3a
Tripoli80 <tripoli80@gmail.com>
2023-01-09 10:51:15 +0000

    first commit

--- src/midelware/cache.js
+++ src/midelware/cache.js
@@ -5,11 +5,12 @@ const verifyCache = (req, res, next) => {
   const {
     body: { ts },
   } = req;
+  console.log("🚀  ts", ts);
   try {
     if (cache.has("ts")) {
-        const tss = cache.get("ts");
-        console.log("🚀  tss/ts", tss, ts)
-        
+      const tss = cache.get("ts");
+      console.log("🚀  tss/ts", tss, ts);
+
       if (tss.includes(Number(ts))) {
         return res.status(210).send({ massage: "already includet" });
       }
