48567d2c6c2ff238e6888ab4e7e0b84a9815106b
Tripoli80 <tripoli80@gmail.com>
2023-01-10 18:40:52 +0000

    first commit

--- src/midelware/checkDublikat.js
+++ src/midelware/checkDublikat.js
@@ -1,25 +1,35 @@
 // const { storage } = require("../helpers/storage");
 
-const checkDublikat = (req, res, next) => {
-  let alldoArr = global.ts;
-  if (alldoArr === undefined) {
-    console.log("🚀  undefined");
-    alldoArr = [];
-  }
-  console.log("🚀1 allts", alldoArr);
+// const checkDublikat = (req, res, next) => {
+//   let alldoArr = global.ts;
+//   if (alldoArr === undefined) {
+//     console.log("🚀  undefined");
+//     alldoArr = [];
+//   }
+//   console.log("🚀1 allts", alldoArr);
 
+//   const {
+//     body: { ts },
+//   } = req;
+
+//   console.log("🚀2 ~ ts", ts, "/", alldoArr);
+
+//   if (alldoArr.includes(Number(ts))) {
+//     console.log("🚀3 +++++++", Number(ts));
+//     return res.status(210).send("already worked");
+//   }
+//   alldoArr.push(Number(ts));
+//   global.ts = alldoArr;
+//   return next();
+// };
+
+const checkDublikat = (req, res, next) => {
   const {
     body: { ts },
   } = req;
 
-  console.log("🚀2 ~ ts", ts, "/", alldoArr);
+  console.log("🚀2 ~ ts", ts);
 
-  if (alldoArr.includes(Number(ts))) {
-    console.log("🚀3 +++++++", Number(ts));
-    return res.status(210).send("already worked");
-  }
-  alldoArr.push(Number(ts));
-  global.ts = alldoArr;
   return next();
 };
 module.exports = { checkDublikat };
