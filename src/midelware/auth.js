69d8f7379e1a871bdacea3150d44f67f30f86b14
Tripoli80 <tripoli80@gmail.com>
2023-01-12 19:10:19 +0000

    first commit

--- src/midelware/auth.js
+++ src/midelware/auth.js
@@ -2,13 +2,10 @@ require("dotenv").config();
 
 const auth = async (req, res, next) => {
   const secret = process.env.SECRET;
-  console.log("🚀 ~ file: auth.js:3 ~ secret", secret);
   const { body } = req;
-    console.log(body["auth[application_token]"]);
-    if (secret === body["auth[application_token]"]) { 
-        console.log("next")
-    }
-      return res.status(200).json({ auth: body["auth[application_token]"] });
+  if (secret === body["auth[application_token]"]) {
+    return next();
+  } else return res.status(401).json();
 };
 
 module.exports = auth;
