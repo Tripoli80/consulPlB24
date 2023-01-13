32c392eb5102f0d89d784a079b6c2b9c8e76341e
Tripoli80 <tripoli80@gmail.com>
2023-01-13 11:34:55 +0000

    first commit

--- src/services/callendar.js
+++ src/services/callendar.js
@@ -18,7 +18,7 @@ const addToCallendar = async ({
     const option = {
       type: "group",
       ownerId: "2",
-      name: `3 Плановая оплата ${name} `,
+      name: ` Плановая оплата ${name} `,
       description: `https://consultcorporated.bitrix24.pl/crm/deal/details/${idDeal}/`,
       from: date,
       to: date,
@@ -40,7 +40,7 @@ const addToCallendar = async ({
         notify: true,
         reinvite: false,
       },
-      UF_CRM_CAL_EVENT: [idDeal],
+      uf_crm_entity: `DEAL_${idDeal}`,
     };
     try {
       const res = await curl("calendar.event.add.json", option);
